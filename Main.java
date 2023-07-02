fda7915db79c8f01d9b6b2303a63e1fba2600766
Vahegr12 <32619497+Vahegr12@users.noreply.github.com>
2023-07-03 01:52:51 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         String path = "C:\\Users\\vaheg\\IdeaProjects\\CPU\\src\\commands.txt";
         CPU cpu = new CPU();
-        cpu.executeProgramFromFile(path);
+        cpu.execute(path);
         cpu.printRegisters();
+        System.out.println(cpu.readMemory((byte)5));
+        System.out.println(cpu.readMemory((byte)10));
     }
 }
