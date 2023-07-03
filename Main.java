8e8ea00d7d800bdb267fe7c9af5ca73fc55a777f
Vahegr12 <32619497+Vahegr12@users.noreply.github.com>
2023-07-04 02:54:33 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String path = "C:\\Users\\vaheg\\IdeaProjects\\CPU\\src\\commands.txt";
         CPU cpu = new CPU();
-        cpu.execute(path);
-        cpu.printRegisters();
-        System.out.println(cpu.readMemory((byte)5));
-        System.out.println(cpu.readMemory((byte)10));
+
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            cpu.decode(line);
+            cpu.dumpMemory();
+            // cpu.execute(); // modify execute function to read instructon from (memory[0] to memory[4])
+        }
     }
 }
